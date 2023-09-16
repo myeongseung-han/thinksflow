c00ee42a6032f6085222814da11cc2dca7393edb
한명승 <hanmyeongseung@hanmyeongseung-ui-MacBookPro.local>
2023-09-16 23:36:50 +0900

    token 제거

--- src/api/octokit.ts
+++ src/api/octokit.ts
@@ -1,8 +1,6 @@
 import { Octokit } from "octokit";
-import { bearerToken } from "../constant";
 
 const octokit = new Octokit({
-    auth: bearerToken,
 });
 
 export default octokit;
--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,3 +1,3 @@
-export const bearerToken = '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';
+export const bearerToken = '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';
 
 export const getIssueListUrl = 'https://api.github.com/repos/angular/angular-cli/issues';
